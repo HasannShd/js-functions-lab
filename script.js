0ef985e3f05b75fbc98d471bfa7b55ac8da854a1
Hasan <hasnshahidd@gmail.com>
2025-06-17 15:01:35 +0300

    done with hw

---
+++ script.js
@@ -0,0 +1,119 @@
+/*
+let age = 14;
+function isAdult(age){
+if (age >= 18){
+    return `Adult`;
+}
+else if {
+    return `minor`;
+}
+}
+
+console.log('Exercise 2 Result:', isAdult(14));
+*/
+
+
+/*
+function isCharAVowel(letter){
+if ( letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u'){
+    return `true`;
+      } 
+    else if {
+        return `false`;
+    }
+}
+
+console.log('Exercise 3 Result:', isCharAVowel("a"));
+*/
+
+
+/*
+function generateEmail(name , domain){
+    return`${name}@${domain}`;
+
+}
+
+console.log('Exercise 4 Result:', generateEmail("hasan", "gmail.com"));
+*/
+
+
+/*
+function greetUser(name , time){
+    if ( time === 'morning'){
+        return`good morning, ${name}`;
+    }
+        else if ( time === 'afternoon'){
+            return`good afternoon, ${name}`;
+        }
+        else if ( time === 'evening'){
+            return`good evening, ${name}`;
+        }
+}
+
+console.log('Exercise 5 Result:', greetUser("hasan", "afternoon"));
+*/
+
+
+/*
+function maxOfThree(num1 , num2 , num3){
+    if (num1 > num2 && num1 > num3){
+        return`num1`;
+    }
+        else if (num1 < num2 && num2 > num3){
+            return`num2`;
+        }
+        else if (num1 < num3 && num2 < num3){
+            return`num3`;
+        }
+}
+
+console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
+*/
+
+
+/*
+function calculateTip(billAmount , tipPercentage){
+    return billAmount * (tipPercentage/100);
+}
+
+console.log('Exercise 7 Result:', calculateTip(50, 20));
+*/
+
+/*
+function convertTemperature(temp , scale){
+    if ( scale === 'C'){
+        return (temp * (9/5)+32);}
+        else if ( scale === 'F'){
+            return (temp - 32) *(5/9);
+        }
+}
+console.log('Exercise 8 Result:', convertTemperature(32, "C"));
+*/
+
+
+/*
+function basicCalculator(num1 , num2 , operation){
+    if (operation === 'add'){
+        return num1 + num2;
+        }
+        else if (operation === 'subtract'){
+            return num1 - num2;
+        }
+            else if (operation === 'multiply'){
+                return num1 * num2;
+            }
+            else if (operation === 'divide'){
+                return num1 / num2;
+            }
+        
+        }
+
+        console.log('Exercise 9 Result:', basicCalculator(10, 5, "multiply"));
+*/
+
+
+
+
+
+
+
